bd3522bfbf5a12c410f515db29d0643110b43e0d
thebluescreen <marius.poncon@gmail.com>
2024-09-09 21:51:12 +0100

    data yeay

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ import requests
 import json
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
 @app.route('/')
